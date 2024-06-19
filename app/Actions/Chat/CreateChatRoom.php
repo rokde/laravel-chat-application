be45a3dc5e1bbc4e58453d1b57e314d9f79e36f0
Robert Kummer <post@robert-kummer.de>
2024-06-19 16:48:38 +0200

    improved the creation of a chatroom

--- app/Actions/Chat/CreateChatRoom.php
+++ app/Actions/Chat/CreateChatRoom.php
@@ -5,9 +5,16 @@ namespace App\Actions\Chat;
 use App\Models\Chat;
 use App\Models\User;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 
 class CreateChatRoom
 {
+    /**
+     * @param \App\Models\User $user
+     * @param \Illuminate\Support\Collection|array{array{id: integer, name: string}} $participants
+     * @param string|null $name
+     * @return \App\Models\Chat
+     */
     public function execute(User $user, Collection $participants, string|null $name = null): Chat
     {
         $participants->prepend($user);
@@ -16,13 +23,15 @@ class CreateChatRoom
             $name = $participants->pluck('name')->implode(', ');
         }
 
-        return tap(Chat::create([
-            'name' => $name,
-        ]), function (Chat $chat) use ($participants) {
-            $participants->unique('id')->each(fn(User $participant) => $chat->participants()->create([
-                'user_id' => $participant->id,
-                'displayName' => $participant->name,
-            ]));
+        return DB::transaction(function () use ($user, $participants, $name) {
+            return tap(Chat::create([
+                'name' => $name,
+            ]), function (Chat $chat) use ($participants) {
+                $participants->unique('id')->each(fn(User|array $participant) => $chat->participants()->create([
+                    'user_id' => data_get($participant, 'id'),
+                    'displayName' => data_get($participant, 'name'),
+                ]));
+            });
         });
     }
 }
