efe02c1a970152ca7caed3cc31038dd850e7d84b
Robert Kummer <post@robert-kummer.de>
2024-06-23 13:59:19 +0200

    display online/offline status in friends list

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -181,7 +181,7 @@ watchEffect((onCleanup) => {
                                                     class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
                                                 <Avatar :name="$page.props.auth.user.name"
                                                         class="mr-2"
-                                                        size="8"/>
+                                                        size.number="8"/>
 
                                                 {{ $page.props.auth.user.name }}
 
