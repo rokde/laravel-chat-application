fe6e9f46df34eb91cc9ff7fec25915a5d37967ed
Robert Kummer <post@robert-kummer.de>
2024-06-26 15:34:54 +0200

    theme switching added

---
+++ resources/js/Components/ThemeSwitcher.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {MoonIcon, SunIcon} from '@heroicons/vue/24/outline/index.js';
+import {useDark, useToggle} from '@vueuse/core';
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+    <button type="button"
+            class="dark:bg-transparent dark:text-gray-500 dark:hover:text-yellow-100"
+            @click="toggleDark()">
+        <MoonIcon v-if="isDark" class="w-6 h-6" />
+        <SunIcon v-else class="w-6 h-6" />
+    </button>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
+
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
